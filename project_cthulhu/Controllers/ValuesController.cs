5e8d35cd832b36ec99b5d4266eb147c40e20c8fe
Matoyak <mwilliam09@gmail.com>
2018-08-18 02:28:34 -0500

    Moving Angular stuff into View folder, Modifying angular.json to build to wwwroot from View;

---
+++ project_cthulhu/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace project_cthulhu.Controllers {
+	[Route("api/[controller]")]
+	public class ValuesController : Controller {
+		// GET: api/<controller>
+		[HttpGet]
+		public IEnumerable<string> Get() {
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET api/<controller>/5
+		[HttpGet("{id}")]
+		public string Get(int id) {
+			return "value";
+		}
+	}
+}
